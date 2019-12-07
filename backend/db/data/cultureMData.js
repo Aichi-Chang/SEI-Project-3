280d319f0252f291e7bb16aeab83389d7ce439c2
SoniaCWeb <sonia.choudhury@hotmail.co.uk>
2019-12-07 13:58:45 +0000

    added films and music

--- backend/db/data/cultureMData.js
+++ backend/db/data/cultureMData.js
@@ -1,15 +1,260 @@
 
 const cultureMData = (user) => {
   return [{
-    album: 'xxxxx',
-    title: 'xxxxxx',
-    image: 'xxx',
-    year: 12323,
-    artist: 'xxxx',
-    rating: 1,
-    user: user[0],
-    comment: []
-  } 
+
+      artist: 'The Weeknd',
+      title: '',
+      album: 'Beauty Behind the Madness',
+      year: 2015,
+      image: '/src/cultureImg/music/The_Weeknd_-_Beauty_Behind_the_Madness.png',
+      summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      rating: 5,
+      comment: [], 
+      user: users[0]
+    },
+{
+      artist: 'Jay-Z',
+      title: '',
+      album: 'The Blueprint',
+      year: 2001,
+      image: '/src/cultureImg/music/The-Blueprint.png',
+      summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      rating: 5,
+      comment: [], 
+      user: users[0]
+    },
+{
+      artist: 'Post Malone',
+      title: '',
+      album: 'Stoney',
+      year: 2015,
+      image: '/src/cultureImg/music/stoney.jpg',
+      summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      rating: 5,
+      comment: [], 
+      user: users[0]
+    },
+{
+      artist: 'Kendrick Lamar',
+      title: '',
+      album: 'DAMN.',
+      year: 2017,
+      image: '/src/cultureImg/music/kendrick-lamar.jpeg',
+      summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      rating: 5,
+      comment: [], 
+      user: users[0]
+    },
+{
+      artist: 'Drake',
+      title: '',
+      album: 'Scorpion',
+      year: 2018,
+      image: '/src/cultureImg/music/Scorpian-drake.jpg',
+      summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      rating: 5,
+      comment: [], 
+      user: users[0]
+    },
+{
+      artist: 'Adele',
+      title: '',
+      album: '21',
+      year: 2011,
+      image: '/src/cultureImg/music/Adele-21.png',
+      summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      rating: 5,
+      comment: [], 
+      user: users[0]
+    },
+{
+      artist: 'Michael Jackson',
+      title: '',
+      album: 'Thriller',
+      year: 1982,
+      image: '/src/cultureImg/music/thriller-michael-jackson.jpg',
+      summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      rating: 5,
+      comment: [] ,
+      user: users[0]
+    },
+{
+      artist: 'The Clash',
+      title: '',
+      album: 'London Calling',
+      year: 1979,
+      image: '/src/cultureImg/music/the-clash-london-calling.jpg',
+      summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      rating: 5,
+      comment: [], 
+      user: users[0]
+    },
+{
+      artist: 'Backstreet Boys',
+      title: '',
+      album: 'Millenium',
+      year: 1999,
+      image: '/src/cultureImg/music/backstreet-boys-Millennium_cover.jpg',
+      summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      rating: 5,
+      comment: [], 
+      user: users[0]
+    },
+
+{
+      artist: 'Bishop Briggs',
+      title: '',
+      album: 'CHAMPION',
+      year:  2019,
+      image: '/src/cultureImg/music/bishop-briggs.jpg',
+      summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      rating: 5,
+      comment: [], 
+      user: users[0]
+    },
+{
+      artist: 'The Killers',
+      title: '',
+      album: 'Hot Fuss',
+      year: 2004,
+      image: '/src/cultureImg/music/The-Killers-Hot-Fuss.jpg',
+      summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      rating: 5,
+      comment: [], 
+      user: users[0]
+    },
+{
+      artist: 'Panic At the Disco',
+      title: '',
+      album: 'A Fever You Can\'t Sweat Out',
+      year: 2005,
+      image: '/src/cultureImg/music/panic-at-the-disco.jpg',
+      summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      rating: 5,
+      comment: [],
+      user: users[0]
+    },
+{
+      artist: 'Kings of Leon',
+      title: '',
+      album: 'Only By The Night',
+      year: 2008,
+      image: '/src/cultureImg/music/KingsOfLeon-OnlyByTheNightCover.png',
+      summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      rating: 5,
+      comment: [], 
+      user: users[0]
+    },
+{
+      artist: 'Red Hot Chill Peppers',
+      title: '',
+      album: 'Californication',
+      year: 1999,
+      image: '/src/cultureImg/music/RedHotChiliPeppersCalifornication.jpg',
+      summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      rating: 5,
+      comment: [], 
+      user: users[0]
+    },
+{
+      artist: 'Beyoncé',
+      title: '',
+      album: '4',
+      year: 2011,
+      image: '/Use/src/cultureImg/music/Beyoncé-4.png',
+      summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      rating: 5,
+      comment: [], 
+      user: users[0]
+    },
+{
+      artist: 'Nirvana',
+      title: '',
+      album: 'Nevermind',
+      year: 1991,
+      image: '/src/cultureImg/music/NirvanaNevermindalbumcover.jpg',
+      summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      rating: 5,
+      comment: [], 
+      user: users[0]
+    },
+{
+      artist: 'BTS',
+      title: '',
+      album: 'Map of the Soul: Persona',
+      year: 2019,
+      image: '/src/cultureImg/music/bts-map-of-soul.jpeg',
+      summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      rating: 5,
+      comment: [] 
+      user: users[0]
+    },
+{
+      artist: 'Bob Dylan',
+      title: '',
+      album: 'The Freewheelin\' Bob Dylan',
+      year: 1963,
+      image: '/src/cultureImg/music/bob-dylan.jpg',
+      summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      rating: 5,
+      comment: [] 
+      user: users[0]
+    },
+{
+      artist: 'Tzusing',
+      title: '東方不敗',
+      album: '',
+      year: 2015,
+      image: '/src/cultureImg/music/tzuzing.jpg',
+      summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      rating: 5,
+      comment: [] 
+      user: users[0]
+    },
+{
+      artist: 'Lescop',
+      title: '',
+      album: 'Lescop',
+      year: 2016,
+      image: '/src/cultureImg/music/lescop.jpg',
+      summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      rating: 5,
+      comment: [], 
+      user: users[0]
+    },
+{
+      artist: 'Lauv',
+      title: '',
+      album: 'Lauv EP: JAPAN EDITION',
+      year: 2018,
+      image: '',
+      summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      rating: 5,
+      comment: [],
+      user: users[0]
+    }, 
+    {
+      artist: 'Taylor Swift',
+      title: '',
+      album: 'Lover',
+      year: 2019,
+      image: '/src/cultureImg/music/taylor-swift.jpeg',
+      summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      rating: 5,
+      comment: [],
+      user: users[0]
+    },
+    {
+      artist: 'Lana Del Rey',
+      title: '',
+      album: 'Born to Die',
+      year: 2012,
+      image: '/src/cultureImg/music/lana-BornToDieAlbumCover.png',
+      summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      rating: 5,
+      comment: [],
+      user: users[0]
+    }
   ]
 }
 
