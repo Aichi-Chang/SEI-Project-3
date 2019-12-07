22b623e5fbc3631f7157b565fd585a8cc7f7b2b2
Aichi Chang <phoebexxxc@gmail.com>
2019-12-07 21:57:28 +0000

    community seeding -Aichi -07/12/19 -22:00

--- backend/db/data/CommuData.js
+++ backend/db/data/CommuData.js
@@ -1,23 +1,5 @@
 module.exports = [
   {
-    title: 'CAFE: REGENCY CAFÉ',
-    subTitle: 'Must order: The Full English, obviously',
-    image: 'frontend/src/images/commuImg/regency-cafe.jpeg',
-    text: 'It’s easy sometimes to forget that London’s café culture wasn’t invented in 2007 by an influx of avocado-smashing Aussies. Greasy spoons have long been a central part of life in the capital and nowhere keeps the flame burning better than the Regency Café near Tate Britain. Think classic white-tiled walls and formica tables, with a roaring tea-urn. Grab yourself a spot in amongst the dust-covered labourers and tuck into sausages, eggs and chips while owner Claudia Perotti barks out the orders in her booming baritone. They don’t, it probably goes without saying, do single-estate pour-over.',
-    lng: -0.132213,
-    lat: 51.494188,
-    address: 'Regency Café, 17-19 Regency Street, SW1P 4BY'
-  },
-  {
-    title: 'CAFE: GINGER & WHITE',
-    subTitle: 'Must order: the soft-boiled eggs and spend an extra £3 to take the egg cosy home with you',
-    image: 'frontend/src/images/commuImg/ginger-and-white.png',
-    text: 'The soft-boiled eggs come in little hand-knitted hats at this Hampstead café, perfectly located in the heart of the village. The interior is a lovely white box with stripped back retro-industrial fittings and the clientele is refreshingly varied for the neighbourhood: guys in high-vis as well as the handbag brigade. Prep yourself for a stomp on the Heath with smashed avocado and goat’s cheese on toasted sourdough and a Square Mile flat white at a hidden-away corner table.',
-    lng: -0.177779,
-    lat: 551.555678,
-    address: 'Ginger & White, 4a-5a Perrin\'s Court, NW3 1QS'
-  },
-  {
     title: 'CAFE: ESTERS',
     subTitle: 'Must order: the imperious Blondie is just exactly right in every way',
     image: 'frontend/src/images/commuImg/esters.jpg',
@@ -27,22 +9,31 @@ module.exports = [
     address: 'Esters, 55 Kynaston Road, N16 0EB'
   },
   {
-    title: 'CAFE: BRUNSWICK EAST BAKEHOUSE',
-    subTitle: 'Must order: The black sesame and turmeric bread is made with heirloom turmeric imported direct from a social enterprise in India',
-    image: 'frontend/src/images/commuImg/brunswick-east-bakehouse.jpeg',
-    text: 'Following on from their cult Brunswick East café, Aussie sisters Natasha Bacon and Shaunae England have opened a second site, Brunswick East Bakehouse in Hackney Downs Studios, and it might just be – whisper it – an improvement on the original. Here, they are blessed with simpatico neighbours (greenery from plant shop Kill the Closest Snake fill the windows of the light-filled shared space), and, crucially, there’s room for their very own bread oven. And what wonders they have worked with it! The breakfast bruschetta is a slab of sourdough heaving with avocado, basil and cashew cream, and the signature chili poached eggs come on a flatbread made to order. The cake counter heaves with delights like orange and cardamom scrolls and blueberry and coconut friands. The Coffee is Square Mile. And the whole place buzzes with the feel of A Good Thing.',
-    lng: -0.067115,
-    lat: 51.554761,
-    address: 'Brunswick East Bakehouse, Hackney Downs Studios, 17 Amhurst Terrace, E8 2BT'
+    title: 'SHOP: BYO-PLASTIC-FREE SHOP',
+    subTitle: '',
+    image: 'frontend/src/images/commuImg/byo.jpeg',
+    text: 'BYO - Bring Your Own, is a gorgeous plastic-free shop. Conscious residents of South East London will now be able to shop plastic-free by bringing their own reusable containers to BYO and filling it with the amount you need of each item - helping customers to reduce their environmental footprint and at the same time cut down on food waste. Shoppers can choose from a variety of quality organic grains, pulses, nuts, dried fruits, pickles, spices and vegan chocolate buttons! You can also choose oils like Argan, sesame and chilli olive oil as well as household goods, including cleaning products and body care essentials all without the plastic wrapping. BYO also sell a selection of reusable items like eco coffee cups and water bottles. ',
+    lng: -0.056797,
+    lat: 51.466056,
+    address: '147 Evelina Rd, London SE15 3HB'
   },
   {
-    title: 'CAFE: ONE & ALL',
-    subTitle: 'Must order: The roasted veg salad with spicy chickpeas is a ridiculous bargain at £4',
-    image: 'frontend/src/images/commuImg/one-and-all.jpg',
-    text: 'Aware that Peckham’s not exactly a neighbourhood awash with cash, owner Maddy Beevers has put the emphasis very much on affordability at this lovely plywood-decked neighbourhood café; a cup of tea costs a quid and you can pick up a bowl of their flavour-packed fattoush for just £3.50. There’s a pay it forward scheme, which allows you to buy a drink for someone less able to afford it at half price (she’ll pick up the other half). And there’s a clothes swap in the back. The coffee is sourced from Café Feminino, a women farmers’ collective in Peru. And everything about the place is sunny and lovely.',
-    lng: -0.06676,
-    lat: 51.465384,
-    address: 'One & All, 28 Peckham Rye, SE15 4JR'
+    title: 'CAFE: REGENCY CAFÉ',
+    subTitle: 'Must order: The Full English, obviously',
+    image: 'frontend/src/images/commuImg/regency-cafe.jpeg',
+    text: 'It’s easy sometimes to forget that London’s café culture wasn’t invented in 2007 by an influx of avocado-smashing Aussies. Greasy spoons have long been a central part of life in the capital and nowhere keeps the flame burning better than the Regency Café near Tate Britain. Think classic white-tiled walls and formica tables, with a roaring tea-urn. Grab yourself a spot in amongst the dust-covered labourers and tuck into sausages, eggs and chips while owner Claudia Perotti barks out the orders in her booming baritone. They don’t, it probably goes without saying, do single-estate pour-over.',
+    lng: -0.132213,
+    lat: 51.494188,
+    address: 'Regency Café, 17-19 Regency Street, SW1P 4BY'
+  },
+  {
+    title: 'BAR: PATRON-CAVE À MANGER',
+    subTitle: '',
+    image: 'frontend/src/images/commuImg/patron-cave-a-manger.jpg',
+    text: 'With a stunning marble-topped bar, beautiful decor and just the right lighting, Patron is the perfect French wine bar serving delicious seasonal food alongside fantastic signature dishes like duck confit and ongolet steak. Bookings aren’t taken at the bar so it’s possible to get a seat at short notice and it’s also a great spot for a cocktail or glass of wine. Owners Jean-Francois and Tanzi have created a little oasis on this unlikely stretch of road which is definitely worth a visit.',
+    lng: -0.1408238,
+    lat: 51.5526938,
+    address: '26 Fortess Road Kentish Town London NW5 2HB'
   },
   {
     title: 'RESTAURANT: BUN HOUSE',
@@ -63,6 +54,15 @@ module.exports = [
     address: 'Frank\'s Café, Bold Tendencies, 7th-10th Floor Multi Storey Car Park, 95A Rye Lane, London SE15 4ST'
   },
   {
+    title: 'CAFE: GINGER & WHITE',
+    subTitle: 'Must order: the soft-boiled eggs and spend an extra £3 to take the egg cosy home with you',
+    image: 'frontend/src/images/commuImg/ginger-and-white.png',
+    text: 'The soft-boiled eggs come in little hand-knitted hats at this Hampstead café, perfectly located in the heart of the village. The interior is a lovely white box with stripped back retro-industrial fittings and the clientele is refreshingly varied for the neighbourhood: guys in high-vis as well as the handbag brigade. Prep yourself for a stomp on the Heath with smashed avocado and goat’s cheese on toasted sourdough and a Square Mile flat white at a hidden-away corner table.',
+    lng: -0.177779,
+    lat: 551.555678,
+    address: 'Ginger & White, 4a-5a Perrin\'s Court, NW3 1QS'
+  },
+  {
     title: 'SHOP: TOKYO BIKE',
     subTitle: '',
     image: 'frontend/src/images/commuImg/tokyo-bike.jpg',
@@ -81,13 +81,13 @@ module.exports = [
     address: '72 Lordship Ln, East Dulwich, London SE22 8HF'
   },
   {
-    title: 'SHOP: BYO: PLASTIC-FREE SHOP',
-    subTitle: '',
-    image: 'frontend/src/images/commuImg/byo.jpeg',
-    text: 'BYO - Bring Your Own, is a gorgeous plastic-free shop. Conscious residents of South East London will now be able to shop plastic-free by bringing their own reusable containers to BYO and filling it with the amount you need of each item - helping customers to reduce their environmental footprint and at the same time cut down on food waste. Shoppers can choose from a variety of quality organic grains, pulses, nuts, dried fruits, pickles, spices and vegan chocolate buttons! You can also choose oils like Argan, sesame and chilli olive oil as well as household goods, including cleaning products and body care essentials all without the plastic wrapping. BYO also sell a selection of reusable items like eco coffee cups and water bottles. ',
-    lng: -0.056797,
-    lat: 51.466056,
-    address: '147 Evelina Rd, London SE15 3HB'
+    title: 'CAFE: ONE & ALL',
+    subTitle: 'Must order: The roasted veg salad with spicy chickpeas is a ridiculous bargain at £4',
+    image: 'frontend/src/images/commuImg/one-and-all.jpg',
+    text: 'Aware that Peckham’s not exactly a neighbourhood awash with cash, owner Maddy Beevers has put the emphasis very much on affordability at this lovely plywood-decked neighbourhood café; a cup of tea costs a quid and you can pick up a bowl of their flavour-packed fattoush for just £3.50. There’s a pay it forward scheme, which allows you to buy a drink for someone less able to afford it at half price (she’ll pick up the other half). And there’s a clothes swap in the back. The coffee is sourced from Café Feminino, a women farmers’ collective in Peru. And everything about the place is sunny and lovely.',
+    lng: -0.06676,
+    lat: 51.465384,
+    address: 'One & All, 28 Peckham Rye, SE15 4JR'
   },
   {
     title: 'SHOP: FOREST',
@@ -115,5 +115,41 @@ module.exports = [
     lng: -0.1135442,
     lat: 51.4621978,
     address: 'Brixton Village, Unit 37-38 Coldharbour Ln, Brixton, London SW9 8PR'
-  }        
+  },
+  {
+    title: 'BAR: MOTHER KELLY\'S',
+    subTitle: '',
+    image: 'frontend/src/images/commuImg/mother-kellys.jpg',
+    text: 'Mother Kelly’s is the beer paradise, inspired by New-York tap rooms and situated in one of the railway arches of Paradise Row. You have the choice between 23 beers on the tap (which they change pretty much everyday) and a HUGE selection of bottles to pick up from the fridges. Lagers, IPA, stouts, Belgian krieks, brown beers, black beers, cider... there are beers for everyone to discover. The bottles are also available to take-away, so you can do your beer shopping here as well. The place is super convivial, on top of the beers, you can enjoy cheese, meat, vegetable and chocolate boards! There is plenty of space inside and outside on the terrace, perfect to come with a big group of friends.',
+    lng: -0.0561730,
+    lat: 51.5283630,
+    address: '251 Paradise Row Bethnal Green London E2 9BU'
+  },
+  {
+    title: 'SHOP: ARTWORDS',
+    subTitle: '',
+    image: 'frontend/src/images/commuImg/artwords.jpg',
+    text: 'This is our favourite shop for books and magazines when we\'re in need of cool reference and inspiration pieces. We think it\'s important to support local, independent bookshops like this one and given its close proximity to our office, we can get our hands on the goods faster than we could from Amazon anyway!',
+    lng: -0.0798289,
+    lat: 51.5262188,
+    address: '69 Rivington Street London EC2A 3AY'
+  },
+  {
+    title: 'CAFE: BRUNSWICK EAST BAKEHOUSE',
+    subTitle: 'Must order: The black sesame and turmeric bread is made with heirloom turmeric imported direct from a social enterprise in India',
+    image: 'frontend/src/images/commuImg/brunswick-east-bakehouse.jpeg',
+    text: 'Following on from their cult Brunswick East café, Aussie sisters Natasha Bacon and Shaunae England have opened a second site, Brunswick East Bakehouse in Hackney Downs Studios, and it might just be – whisper it – an improvement on the original. Here, they are blessed with simpatico neighbours (greenery from plant shop Kill the Closest Snake fill the windows of the light-filled shared space), and, crucially, there’s room for their very own bread oven. And what wonders they have worked with it! The breakfast bruschetta is a slab of sourdough heaving with avocado, basil and cashew cream, and the signature chili poached eggs come on a flatbread made to order. The cake counter heaves with delights like orange and cardamom scrolls and blueberry and coconut friands. The Coffee is Square Mile. And the whole place buzzes with the feel of A Good Thing.',
+    lng: -0.067115,
+    lat: 51.554761,
+    address: 'Brunswick East Bakehouse, Hackney Downs Studios, 17 Amhurst Terrace, E8 2BT'
+  },
+  {
+    title: 'RESTAURANT: 10 GREEK STREET',
+    subTitle: '',
+    image: 'frontend/src/images/commuImg/10greekst.jpg',
+    text: 'This restaurant has a simple, monochrome interior with tiled features and teak chairs, and an open kitchen at the back. There\'s a daily changing menu with specials chalked up on a blackboard, and the food is seriously delicious (if the burrata is on the menu, have it). No bookings so be prepared to queue.',
+    lng: -0.1309956,
+    lat: 51.5144593,
+    address: '10 Greek Street, London W1D 4DH'
+  }      
 ]
