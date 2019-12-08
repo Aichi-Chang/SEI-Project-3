5d953639e701c368def089d481d60cdd2be5b519
SoniaCWeb <sonia.choudhury@hotmail.co.uk>
2019-12-08 18:24:05 +0000

    added more music items- sonia, 18:23

--- backend/db/data/cultureMData.js
+++ backend/db/data/cultureMData.js
@@ -254,7 +254,73 @@ const cultureMData = (user) => {
       rating: 5,
       comment: [],
       user: users[0]
-    }
+    },
+    {
+      artist: 'Childish Gambino',
+      title: '',
+      album: 'Awaken, My Love!',
+      year: 2016,
+      image: '/src/cultureImg/music/Childish-Gambino-Awaken-My-Love.png',
+      summary: 'Donald Glover silenced any remaining Gambino doubters through his 2016 reinvention as a soul and funk singer from the 1970s. The pairing of Glover’s powerful vocals (rapturous opener ‘Me and Your Mama’ and the frenetic ‘Riot’ being two fine examples) and Ludwig Göransson’s lush production paid wonderful homage to the greats, while the foot-tapping twilight groove of ‘Redbone’ – which won a Grammy and went global after soundtracking the opening scene of Get Out – is guaranteed to live on for many years to come',
+      rating: 5,
+      comment: [],
+      user: users[0]
+    },
+    {
+      artist: 'Rihanna',
+      title: '',
+      album: 'Anti',
+      year: 2016,
+      image: '/src/cultureImg/music/Rihanna-Anti.png',
+      summary: 'Rihanna’s eighth studio album eschewed the big pop bangers that her peers – and Rihanna to be fair – usually relied on and instead opted for a more forward-thinking, experimental approach. Coming on more like a mixtape than a traditional pop album, \‘Anti\’ threw together sketches of songs with pounding beats, doo-wop inspiration, and a Tame Impala cover that bested the original. As the wait continues for \‘R9\’, it stands as a shining example of the bold brilliance RiRi\’s DGAF attitude sparks.',
+      rating: 5,
+      comment: [],
+      user: users[0]
+    },
+    {
+      artist: 'A$AP Rocky',
+      title: '',
+      album: 'Long.Live.ASAP',
+      year: 2013,
+      image: '/src/cultureImg/music/asap-rocky.jpg',
+      summary: 'A crack of thunder set the stark and ominous tone for A$AP Rocky’s debut studio album before a sharp quip: “I thought I’d probably die in prison/Expensive taste in women”. The Harlem-born and raised rapper had a few false starts with this record, one that was supposedly the product of an eye-watering $3million deal. An absolute landscape of ideas, innovation and ambition – to say it was worth the wait is a massive understatement.',
+      rating: 5,
+      comment: [],
+      user: users[0]
+    },
+    {
+      artist: 'Solange',
+      title: '',
+      album: 'A Seat At The Table',
+      year: 2016,
+      image: '/src/cultureImg/music/Solange-A-Seat-at-the-Table.png',
+      summary: 'On her third record, Solange Knowles invited her listeners to pull up a chair, and listen to her lived experience as a black woman in the United States of America. Touching on both pain and healing, there’s an intense warmth to the soundscape of \‘A Seat At The Table\’. \“I’ve got a lot to be mad about,\” she sings atop gentle twinkles of piano – a fire burns at this astounding record’s heart.',
+      rating: 5,
+      comment: [],
+      user: users[0]
+    },
+    {
+      artist: 'Lorde',
+      title: '',
+      album: 'Pure Heroin',
+      year: 2013,
+      image: '/src/cultureImg/music/Lorde-Pure-Heroine.png',
+      summary: 'Lorde never shied away from the influences she grew up with – Florence + The Machine, James Blake, Lana Del Rey – but her youthful wisdom was unique from the start. ‘Pure Heroine’ sings bravely of teenage boredom and ambition, finding adrenaline in the mundanity. She laid rich and hypnotic vocals over sirens, attacking the clutches of fame and celebrating the nighttime dreams of house parties going nowhere. It was the apex of teen-pop and finally good enough for adults.',
+      rating: 5,
+      comment: [],
+      user: users[0]
+    },
+    {
+      artist: 'Daft Punk',
+      title: '',
+      album: 'Random Access Memories',
+      year: 2013,
+      image: '/src/cultureImg/music/daft-punk.jpg',
+      summary: 'We thought the robots might have abandoned us forever. Instead, eight years after third album proper ‘Human After All’, they returned to blow our minds. They brought with them a stellar supporting cast including Nile Rodgers, Pharrell, Julian Casablancas, Panda Bear and even disco godfather Giorgio Moroder. Together they created a wildly ambitious album that paid tribute to the past while sounding unmistakably like the future. And then, of course, there was ‘Get Lucky’… still the sound of the summer.',
+      rating: 5,
+      comment: [],
+      user: users[0]
+    },
   ]
 }
 
