11877bda1f71440a4b122cc31d1a5ad870b4af77
Aichi Chang <phoebexxxc@gmail.com>
2019-12-10 14:39:38 +0000

    three section components done -Aichi -10/12/19 -14:40

--- frontend/src/components2.0/community/Show.js
+++ frontend/src/components2.0/community/Show.js
@@ -4,12 +4,25 @@ import UseAxios from '../UseAxios'
 const SingleCommunity = (props) => {
   const data = UseAxios(`/api/communities/${props.match.params.id}`)
   // console.log(community)
-  return (
-    <div>
-      <h2>{data.title}</h2>
-      <img src={data.image} />
-      <p>{data.text}</p>
+  return <div className="section">
+    <div className="container">
+      <div className="columns is-multiline">
+        <div className="column is-half-tablet">
+          <p className="title">
+            {data.title}
+          </p>
+          <p className="subtitle">
+            {data.address}
+          </p>
+          <p>
+            {data.text}
+          </p>
+        </div>
+        <div className="column is-half-tablet">
+          <img src={data.image} />
+        </div>
+      </div>
     </div>
-  )
+  </div>
 }
 export default SingleCommunity
